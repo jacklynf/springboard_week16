ec7093af4762f312bf3578027d12bb95f5ac9acd
jackie_aws <jackie@aws_workspace>
2023-05-13 15:07:52 -0700

    passed test, not refactored

--- exercises/exercise_04_pytest/fizzbuzz/test_fizzbuzz.py
+++ exercises/exercise_04_pytest/fizzbuzz/test_fizzbuzz.py
@@ -1,8 +1,17 @@
 import pytest
 
 def fizzbuzz(N):
-    pass
+    if N % 3 == 0:
+        if N % 5 == 0:
+            return "fizzbuzz"
+        return "fizz"
+    elif N % 5 == 0:
+        return "buzz"
+    else:
+        return N
 
 def test_fizzbuzz():
-    pass
-
+    assert fizzbuzz(15) == "fizzbuzz"
+    assert fizzbuzz(3) == "fizz"
+    assert fizzbuzz(5) == "buzz"
+    assert fizzbuzz(7) == 7
