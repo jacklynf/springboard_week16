350544305ea1e0825cce123d89b0320532ad9787
jackie_aws <jackie@aws_workspace>
2023-05-13 15:30:30 -0700

    exercise 4 complete

--- exercises/exercise_04_pytest/fizzbuzz/test_fizzbuzz.py
+++ exercises/exercise_04_pytest/fizzbuzz/test_fizzbuzz.py
@@ -1,17 +1,32 @@
 import pytest
 
+golden_data = [
+    (15, "fizzbuzz"),
+    (3, "fizz"),
+    (5, "buzz"),
+    (7, 7),
+    (12, "fizz"),
+    (20, "buzz"),
+    (30, "fizzbuzz"),
+    (100, "buzz"),
+    (101, 101),
+]
+
+@pytest.fixture(params=golden_data)
+def my_fixture(request):
+    return request.param
+
 def fizzbuzz(N):
-    if N % 3 == 0:
-        if N % 5 == 0:
-            return "fizzbuzz"
+    if N % 15 == 0:
+        return "fizzbuzz"
+    elif N % 3 == 0:
         return "fizz"
     elif N % 5 == 0:
         return "buzz"
     else:
         return N
 
-def test_fizzbuzz():
-    assert fizzbuzz(15) == "fizzbuzz"
-    assert fizzbuzz(3) == "fizz"
-    assert fizzbuzz(5) == "buzz"
-    assert fizzbuzz(7) == 7
+def test_fizzbuzz(my_fixture):
+    input_val, output = my_fixture
+    assert fizzbuzz(input_val) == output
+    
